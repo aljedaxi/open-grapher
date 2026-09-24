dfdc04510fdeea93fd2b2941e81c5aa02c700a13
daxi <aljedaxi@pm.me>
2026-09-24 15:02:59 -0600

    build: i'm on that v on that handsome nix shit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, self, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [nodejs];
+        shellHook = ''exec zsh'';
+      };
+      packages.default = pkgs.buildNpmPackage {
+        dontNpmBuild = true;
+        name = "open-grapher";
+        npmDepsHash = "sha256-71qi5FmVVbWfCwmefhHKZfzSHFDf+LsQu/q13zPl0ZI=";
+        src = ./.;
+      };
+      apps.default = {
+	      type = "app";
+	      program = "${self.packages."${system}".default}/lib/node_modules/open-grapher/main.js";
+      };
+    });
+}
